3fb9cd49c92f04d168370aaef2620fe83fd7d917
Joe Hebert <raphaelhebert18@gmail.com>
2021-07-10 20:21:41 +0700

    restyled

--- github-user-card/src/components/User.js
+++ github-user-card/src/components/User.js
@@ -2,11 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const UserCard = styled.div`
-    width: 25%;
+    width: 80%;
     margin: 15px auto;
     border: 2px solid grey;
     border-radius: 3px;
-    background: #e8980d;
+    background: lightgrey;
     display: flex;
     flex-flow: column nowrap;
     justify-content: space-evenly;
@@ -14,28 +14,49 @@ const UserCard = styled.div`
 `
 
 const Image = styled.img`
-    margin: auto;
-    border: 4px solid grey;
+    margin: 15px 15px 0 15px;
+    border: 4px solid lightgrey;
     border-radius: 50%;
-    max-width: 95%;
+    max-width: 30%;
     height: auto;
+    object-fit: cover;
     `
 const Graph = styled.div`
-    padding: 15px;
+    width: 100%;
+    padding: 0 0 15px 0;
     display: flex;
     flex-flow: column nowrap;
     justify-content: space-around;
     align-items: center;
+    background:lightgrey;
 `
 
 const Details = styled.div`
     margin: 2% auto;
-    background: #e0b970;
-    padding: 5px;
+    background: lightgrey;
+    padding: 10px;
     width: 85%;
     border: 1px solid grey;
     text-align: center;
+`
+const Graphimg = styled.img`
+    max-width: 95%;
+    height: auto;
+`
+const Top = styled.div`
+    border-radius: 5px;
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    background: grey;
+    padding-bottom: 15px;
+`
+const Topdetails = styled.div`
     
+    width: 80%;
+    margin: auto;
+    display: flex;
+    flex-flow: column nowrap;
 `
 
 class User extends React.Component{
@@ -47,17 +68,22 @@ class User extends React.Component{
         return(this.props.userData? 
             <>
                 <UserCard>
-                    <Image src={this.props.userData.avatar_url} />
-                    <Details>{this.props.userData.login}</Details>
-                    <Details>Company: {this.props.userData.company}</Details>
-                    <Details>Location: {this.props.userData.location}</Details>
-                    <Details>Followers: {this.props.userData.followers}</Details>
-                    <Details><a href={this.props.userData.html_url}> Github </a></Details>
+                    <Top>
+                        <Image src={this.props.userData.avatar_url} />
+                        <Topdetails>
+                            <Details>{this.props.userData.login}</Details>
+                            <Details>Company: {this.props.userData.company}</Details>
+                            <Details>Location: {this.props.userData.location}</Details>
+                            <Details>Followers: {this.props.userData.followers}</Details>
+                            <Details><a href={this.props.userData.html_url}> Github </a></Details>
+                        </Topdetails>
+                    </Top>
+                    <Graph>
+                        <h2>Contributions</h2>
+                        <Graphimg src={graph}/>
+                    </Graph>
                 </UserCard>
-                <Graph>
-                    <h2>Contributions</h2>
-                    <img src={graph}/>
-                </Graph>
+                
             </>: "no user found"
         )   
 
