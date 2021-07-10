ff0046968a881668db8153da109619e5f0c1b40b
Joe Hebert <raphaelhebert18@gmail.com>
2021-07-10 08:48:16 +0700

    added graph

--- github-user-card/src/components/User.js
+++ github-user-card/src/components/User.js
@@ -6,7 +6,6 @@ const UserCard = styled.div`
     margin: 15px auto;
     border: 2px solid grey;
     border-radius: 3px;
-    padding: 10px;
     background: #e8980d;
     display: flex;
     flex-flow: column nowrap;
@@ -21,28 +20,50 @@ const Image = styled.img`
     max-width: 95%;
     height: auto;
     `
+const Graph = styled.div`
+    padding: 15px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-around;
+    align-items: center;
+`
+
+const Details = styled.div`
+    margin: 2% auto;
+    background: #e0b970;
+    padding: 5px;
+    width: 85%;
+    border: 1px solid grey;
+    text-align: center;
+    
+`
 
 class User extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
-        return(
+        const graph = `https://grass-graph.moshimo.works/images/${this.props.userData.login}.png`
+        return(this.props.userData? 
             <>
-            <UserCard>
-                <Image src={this.props.userData.avatar_url} />
-                <p>{this.props.userData.login}</p>
-                <p>since: {this.props.userData.created_at.slice(0,10)}</p>
-                <p>Company: {this.props.userData.company}</p>
-                <p>Location: {this.props.userData.location}</p>
-                <p>Followers: {this.props.userData.followers}</p>
+                <UserCard>
+                    <Image src={this.props.userData.avatar_url} />
+                    <Details>{this.props.userData.login}</Details>
+                    <Details>Company: {this.props.userData.company}</Details>
+                    <Details>Location: {this.props.userData.location}</Details>
+                    <Details>Followers: {this.props.userData.followers}</Details>
+                    <Details><a href={this.props.userData.html_url}> Github </a></Details>
+                </UserCard>
+                <Graph>
+                    <h2>Contributions</h2>
+                    <img src={graph}/>
+                </Graph>
+            </>: "no user found"
+        )   
 
-                <a href={this.props.userData.html_url}> Github </a>
-            </UserCard>
-            <p>{JSON.stringify(this.props.userData)}</p>
-            </>
-
-        )
+        
+            
+        
     }
 }
 
